8f0e1b1093af032b463dc407399468c79fab6292
bmuradova17974 <Bmuradova17974@ada.edu.az>
2024-07-04 21:51:41 +0400

    creating website

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}"
+    );
 app.Run();
